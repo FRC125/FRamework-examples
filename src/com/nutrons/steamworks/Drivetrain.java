986808d5d1c73be2ef8ffcb1acfbf6f0ca8bf3f7
Asher Gottlieb <totally.ashes@gmail.com>
2017-01-23 20:20:52 -0500

    changed to remove boilerplate and use new framework changes

--- src/com/nutrons/steamworks/Drivetrain.java
+++ src/com/nutrons/steamworks/Drivetrain.java
@@ -1,30 +1,31 @@
 package com.nutrons.steamworks;
 
-import com.nutrons.FRamework.InputFactory;
-import com.nutrons.FRamework.OutputFactory;
-import com.nutrons.FRamework.Subsystem;
-import com.nutrons.FRamework.consumers.LoopControllerEvent;
-import com.nutrons.FRamework.consumers.LoopFollowEvent;
-import com.nutrons.FRamework.consumers.RunAtPowerEvent;
+import com.nutrons.framework.Subsystem;
+import com.nutrons.framework.consumers.ControllerEvent;
+import com.nutrons.framework.consumers.FollowEvent;
+import com.nutrons.framework.consumers.RunAtPowerEvent;
+import com.nutrons.framework.factories.InputManager;
+import com.nutrons.framework.factories.OutputFactory;
+import com.nutrons.framework.factories.OutputManager;
 import io.reactivex.Flowable;
 import static java.lang.Math.abs;
 
 public class Drivetrain implements Subsystem {
 
-  private final Flowable<LoopControllerEvent> y1;
-  private final Flowable<LoopControllerEvent> y2;
+  private final Flowable<ControllerEvent> y1;
+  private final Flowable<ControllerEvent> y2;
 
   public Drivetrain() {
-    this.y1 = deadzone(InputFactory.instance().controllerY(0)).map((x) -> new RunAtPowerEvent(x));
-    this.y2 = deadzone(InputFactory.instance().controllerY2(0)).map((x) -> new RunAtPowerEvent(-x));
+    this.y1 = deadzone(InputManager.factory().controllerY(0)).map((x) -> new RunAtPowerEvent(x));
+    this.y2 = deadzone(InputManager.factory().controllerY2(0)).map((x) -> new RunAtPowerEvent(-x));
   }
 
   @Override
   public void registerSubscriptions() {
-    OutputFactory out = OutputFactory.instance();
-    Flowable.just(new LoopFollowEvent(RobotMap.LEFT_DRIVE_MOTOR_A))
+    OutputFactory out = OutputManager.factory();
+    Flowable.just(new FollowEvent(RobotMap.LEFT_DRIVE_MOTOR_A))
         .subscribe(out.motor(RobotMap.LEFT_DRIVE_MOTOR_B));
-    Flowable.just(new LoopFollowEvent(RobotMap.RIGHT_DRIVE_MOTOR_A))
+    Flowable.just(new FollowEvent(RobotMap.RIGHT_DRIVE_MOTOR_A))
         .subscribe(out.motor(RobotMap.RIGHT_DRIVE_MOTOR_B));
     y1.subscribe(out.motor(RobotMap.LEFT_DRIVE_MOTOR_A));
     y2.subscribe(out.motor(RobotMap.RIGHT_DRIVE_MOTOR_A));
--- src/com/nutrons/steamworks/RobotBootstrapper.java
+++ src/com/nutrons/steamworks/RobotBootstrapper.java
@@ -1,27 +1,28 @@
 package com.nutrons.steamworks;
 
-import com.ctre.CANTalon;
-import com.nutrons.FRamework.OutputFactory;
-import com.nutrons.FRamework.Robot;
-import com.nutrons.FRamework.StreamManager;
-import com.nutrons.FRamework.consumers.LoopControllerEvent;
-import com.nutrons.FRamework.consumers.Talon;
+import com.nutrons.framework.Robot;
+import com.nutrons.framework.StreamManager;
+import com.nutrons.framework.consumers.ControllerEvent;
+import com.nutrons.framework.consumers.Talon;
+import com.nutrons.framework.factories.OutputManager;
 import io.reactivex.functions.Consumer;
 import java.util.HashMap;
 
 public class RobotBootstrapper extends Robot {
 
   public RobotBootstrapper() {
-    HashMap<Integer, Consumer<LoopControllerEvent>> motors = new HashMap<>();
-    motors.put(RobotMap.LEFT_DRIVE_MOTOR_A, new Talon(new CANTalon(RobotMap.LEFT_DRIVE_MOTOR_A)));
-    motors.put(RobotMap.LEFT_DRIVE_MOTOR_B, new Talon(new CANTalon(RobotMap.LEFT_DRIVE_MOTOR_B)));
-    motors.put(RobotMap.RIGHT_DRIVE_MOTOR_A, new Talon(new CANTalon(RobotMap.RIGHT_DRIVE_MOTOR_A)));
-    motors.put(RobotMap.RIGHT_DRIVE_MOTOR_B, new Talon(new CANTalon(RobotMap.RIGHT_DRIVE_MOTOR_B)));
-    OutputFactory.instance().setControllers(motors);
+    HashMap<Integer, Consumer<ControllerEvent>> motors = new HashMap<>();
+    motors.put(RobotMap.LEFT_DRIVE_MOTOR_A, new Talon(RobotMap.LEFT_DRIVE_MOTOR_A));
+    motors.put(RobotMap.LEFT_DRIVE_MOTOR_B, new Talon(RobotMap.LEFT_DRIVE_MOTOR_B));
+    motors.put(RobotMap.RIGHT_DRIVE_MOTOR_A, new Talon(RobotMap.RIGHT_DRIVE_MOTOR_A));
+    motors.put(RobotMap.RIGHT_DRIVE_MOTOR_B, new Talon(RobotMap.RIGHT_DRIVE_MOTOR_B));
+    OutputManager.factory().setControllers(motors);
 }
 
   @Override
-  protected StreamManager createStreamManager() {
-    return new SubsystemManager(enabledStream(), competitionStream());
+  protected StreamManager provideStreamManager() {
+    StreamManager sm =  new StreamManager(this);
+    sm.registerSubsystem(new Drivetrain());
+    return sm;
   }
 }
