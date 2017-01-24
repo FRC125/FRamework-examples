3217984ac82c8617788166a4eaabcd7fb9d0640c
Asher Gottlieb <totally.ashes@gmail.com>
2017-01-23 20:46:17 -0500

    removed some instantiation boilerplate

--- src/com/nutrons/steamworks/RobotBootstrapper.java
+++ src/com/nutrons/steamworks/RobotBootstrapper.java
@@ -2,26 +2,26 @@ package com.nutrons.steamworks;
 
 import com.nutrons.framework.Robot;
 import com.nutrons.framework.StreamManager;
-import com.nutrons.framework.consumers.ControllerEvent;
+import com.nutrons.framework.consumers.LoopSpeedController;
 import com.nutrons.framework.consumers.Talon;
 import com.nutrons.framework.factories.OutputManager;
-import io.reactivex.functions.Consumer;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RobotBootstrapper extends Robot {
 
   public RobotBootstrapper() {
-    HashMap<Integer, Consumer<ControllerEvent>> motors = new HashMap<>();
-    motors.put(RobotMap.LEFT_DRIVE_MOTOR_A, new Talon(RobotMap.LEFT_DRIVE_MOTOR_A));
-    motors.put(RobotMap.LEFT_DRIVE_MOTOR_B, new Talon(RobotMap.LEFT_DRIVE_MOTOR_B));
-    motors.put(RobotMap.RIGHT_DRIVE_MOTOR_A, new Talon(RobotMap.RIGHT_DRIVE_MOTOR_A));
-    motors.put(RobotMap.RIGHT_DRIVE_MOTOR_B, new Talon(RobotMap.RIGHT_DRIVE_MOTOR_B));
+    List<LoopSpeedController> motors = new ArrayList<>();
+    motors.add(new Talon(RobotMap.LEFT_DRIVE_MOTOR_A));
+    motors.add(new Talon(RobotMap.LEFT_DRIVE_MOTOR_B));
+    motors.add(new Talon(RobotMap.RIGHT_DRIVE_MOTOR_A));
+    motors.add(new Talon(RobotMap.RIGHT_DRIVE_MOTOR_B));
     OutputManager.factory().setControllers(motors);
-}
+  }
 
   @Override
   protected StreamManager provideStreamManager() {
-    StreamManager sm =  new StreamManager(this);
+    StreamManager sm = new StreamManager(this);
     sm.registerSubsystem(new Drivetrain());
     return sm;
   }
