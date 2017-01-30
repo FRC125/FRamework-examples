aa0e6c6affd2faf1f50af73d6967a9a804af382b
cappuccinocat <xinglydia@gmail.com>
2017-01-30 18:35:02 -0500

    Made pull request changes

--- src/com/nutrons/steamworks/Vision.java
+++ src/com/nutrons/steamworks/Vision.java
@@ -1,30 +1,31 @@
-package com.nutrons.stronghold;
+package com.nutrons.steamworks;
 
 import io.reactivex.Flowable;
 
-public class Vision{
+public class Vision {
     private Flowable<byte[]> dataStream;
     private Flowable<Double[]> dataStreamDouble;
     private Flowable<Double> angle;
     private Flowable<Double> distance;
 
-    Vision(Flowable<byte[]> dataStream){
+    Vision(Flowable<byte[]> dataStream) {
         this.dataStream = dataStream;
         this.dataStreamDouble = this.dataStream
                 .filter(x -> x.length == 10)
                 .map(x -> new String(x, "UTF-8"))
                 .map(x -> x.split(":")).filter(x -> x.length == 2)
-                .map(x -> new Double[]{Double.valueOf(x[0]), Double.valueOf(x[1])}); //Returns a double array[angle, distance]
+                .map(x -> new Double[]{Double.valueOf(x[0]),
+                        Double.valueOf(x[1])}); //Returns a double array[angle, distance]
 
         this.angle = dataStreamDouble.map(x -> x[0]);
         this.distance = dataStreamDouble.map(x -> x[1]);
     }
 
-    public Flowable<Double> getAngle(){
+    public Flowable<Double> getAngle() {
         return this.angle;
     }
 
-    public Flowable<Double> getDistance(){
+    public Flowable<Double> getDistance() {
         return this.distance;
     }
 }
