4410b683509c4e43524af4812b78e65bd625d9aa
AndreasLc1103 <darkninjawolf1@gmail.com>
2017-02-05 15:00:50 -0500

    Added Hopper subsystem

---
+++ src/com/nutrons/steamworks/Hopper.java
@@ -0,0 +1,31 @@
+package com.nutrons.steamworks;
+
+import com.nutrons.framework.Subsystem;
+import com.nutrons.framework.controllers.ControllerEvent;
+import com.nutrons.framework.controllers.RunAtPowerEvent;
+import com.nutrons.framework.controllers.Talon;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import org.reactivestreams.Subscriber;
+
+
+public class Hopper implements Subsystem {
+    private final Flowable<ControllerEvent> runHopper;
+    private final Consumer<ControllerEvent> hopperController;
+
+    /**
+     * HopperController
+     * Tune RunAtPowerEvent
+     */
+    public Hopper(Consumer<ControllerEvent> HopperController) {
+        this.runHopper = Flowable.just(new RunAtPowerEvent(0.9));
+        this.hopperController = HopperController;
+
+    }
+
+
+
+    @Override
+    public void registerSubscriptions() {runHopper.subscribe(hopperController);}
+}
--- src/com/nutrons/steamworks/RobotBootstrapper.java
+++ src/com/nutrons/steamworks/RobotBootstrapper.java
@@ -10,9 +10,13 @@ public class RobotBootstrapper extends Robot {
     private WpiXboxGamepad controller;
     private Talon shooterMotor1;
     private Talon shooterMotor2;
+    private Talon TopHopperMotor;
+    private Talon SpinHopperMotor;
 
     @Override
     protected void constructStreams() {
+        this.TopHopperMotor = new Talon(RobotMap.TOP_HOPPER_MOTOR);
+        this.SpinHopperMotor =  new Talon(RobotMap.SPIN_HOPPER_MOTOR,this.TopHopperMotor);
         this.intakeController = new Talon(RobotMap.INTAKE_MOTOR);
         this.shooterMotor1 = new Talon(RobotMap.SHOOTER_MOTOR_1);
         this.shooterMotor2 = new Talon(RobotMap.SHOOTER_MOTOR_2, this.shooterMotor1);
@@ -25,6 +29,7 @@ public class RobotBootstrapper extends Robot {
         StreamManager sm = new StreamManager(this);
         sm.registerSubsystem(new Shooter(shooterMotor1));
         sm.registerSubsystem(new Feeder(intakeController));
+        sm.registerSubsystem(new Hopper(SpinHopperMotor));
         return sm;
     }
 }
