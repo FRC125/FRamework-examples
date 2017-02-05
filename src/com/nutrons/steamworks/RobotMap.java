537ce4bb7427ddac67bc982a31dcd0ee2b19eeb2
Andreas Leonard-Calcano <darkninjawolf1@gmail.com>
2017-02-05 15:27:09 -0500

    Update RobotMap.java

--- src/com/nutrons/steamworks/RobotMap.java
+++ src/com/nutrons/steamworks/RobotMap.java
@@ -7,6 +7,6 @@ public class RobotMap {
     public static final int INTAKE_MOTOR = 4;
     public static final int SHOOTER_MOTOR_1 = 2;
     public static final int SHOOTER_MOTOR_2 = 3;
-    public static final int TOP_HOPPER_MOTOR = 0;
+    public static final int TOP_HOPPER_MOTOR = 0; // need to change ln 10 and 11 when we get actual port #
     public static final int SPIN_HOPPER_MOTOR = 0;
 }
