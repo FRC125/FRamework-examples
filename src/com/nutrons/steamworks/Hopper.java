c393a79bad13ecc8bd3560828a5d0e9513c5e6d8
AndreasLc1103 <darkninjawolf1@gmail.com>
2017-02-07 18:20:49 -0500

    Fixed TODOs

--- src/com/nutrons/steamworks/Hopper.java
+++ src/com/nutrons/steamworks/Hopper.java
@@ -2,7 +2,7 @@ package com.nutrons.steamworks;
 
 import com.nutrons.framework.Subsystem;
 import com.nutrons.framework.controllers.ControllerEvent;
-import com.nutrons.framework.controllers.RunAtPowerEvent;
+import com.nutrons.framework.controllers.Events;
 import com.nutrons.framework.controllers.Talon;
 import io.reactivex.Flowable;
 import io.reactivex.disposables.Disposable;
@@ -11,16 +11,16 @@ import org.reactivestreams.Subscriber;
 
 
 public class Hopper implements Subsystem {
-    private final static double spinsPower = 0.9;
+    // TODO: tune as needed
+    private final static double SPIN_POWER = 0.9;
     private final Flowable<ControllerEvent> runHopper;
     private final Consumer<ControllerEvent> hopperController;
 
     /**
      * @param hopperController passes in RunAtPowerEvent
-     * @todo Tune spinsPower if needed
      */
     public Hopper(Consumer<ControllerEvent> hopperController) {
-        this.runHopper = Flowable.just(new RunAtPowerEvent(spinsPower));
+        this.runHopper = Flowable.just(Events.power(SPIN_POWER));
         this.hopperController = hopperController;
 
     }
